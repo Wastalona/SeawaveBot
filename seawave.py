5b37a60596d208ddddd79950ea06bd388b0f9831
Aleksey <murzenk314@gmail.com>
2024-08-05 18:32:39 +0300

    Echo bot

---
+++ seawave.py
@@ -0,0 +1,27 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums.parse_mode import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+from icecream import ic
+
+from config import config
+from handlers import router
+
+
+async def main():
+    cfg = config["development"]
+
+    bot = Bot(token=cfg.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    dp = Dispatcher(storage=MemoryStorage())
+    dp.include_router(router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
